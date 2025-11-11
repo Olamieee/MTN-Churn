53f1c158036ea86b5b51c97f425048fbf35dd5ed
Olamieee <alongeola16@gmail.com>
2025-11-11 18:56:36 +0100

    final

--- churn.py
+++ churn.py
@@ -5,7 +5,7 @@ import pandas as pd
 app = Flask(__name__)
 
 model = joblib.load("xg_churn_model.pkl")
-label_encoders = joblib.load("label_encoders.pkl")
+label_encoders = joblib.load("label_encoder.pkl")
 scaler = joblib.load("scaler.pkl")
 
 FEATURE_NAMES = [
